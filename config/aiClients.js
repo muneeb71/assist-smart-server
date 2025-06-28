5de7720eabaafa6f73ccc33b8b848e44e6c6e719
cyhammad <hammad727272@gmail.com>
2025-06-28 23:15:56 +0500

    Update aiClients.js

--- config/aiClients.js
+++ config/aiClients.js
@@ -2,3 +2,17 @@ import { OpenAI } from "openai";
 import Anthropic from "@anthropic-ai/sdk";
 import { GoogleGenAI } from "@google/genai";
 
+export const client = new OpenAI({
+  apiKey:
+    process.env.OPEN_AI_API_KEY,
+});
+ 
+export const anthropic = new Anthropic({
+  apiKey:
+    process.env.ANTHROPIC_API_KEY,
+});
+ 
+export const gemini = new GoogleGenAI({
+  apiKey: 
+    process.env.GOOGLE_GENAI_API_KEY,
+});
