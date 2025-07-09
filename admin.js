2d69c4963d762ed7e2ba97b5d78fa3e036439b52
muneeb71 <ahmedmuneeb183@gmail.com>
2025-07-10 01:11:23 +0500

    feat: admin panel initialized

--- admin.js
+++ admin.js
@@ -1,23 +1,21 @@
 import AdminJS from "adminjs";
 import AdminJSExpress from "@adminjs/express";
-import AdminJSPrisma, { Database, Resource } from "@adminjs/prisma";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import AdminJSPrisma from "@adminjs/prisma";
+import { getModelsToDisplay } from "./lib/adminJsModels.js";
 
 AdminJS.registerAdapter({
-  Database,
-  Resource,
+  Database: AdminJSPrisma.Database,
+  Resource: AdminJSPrisma.Resource,
 });
 
 const adminJs = new AdminJS({
-  databases: [prisma],
   rootPath: "/admin-panel",
   branding: {
     companyName: "Smarthse",
     logo: false,
     softwareBrothers: false,
   },
+  resources: getModelsToDisplay()
 });
 
 const ADMIN = {
@@ -25,15 +23,17 @@ const ADMIN = {
   password: process.env.ADMIN_PASSWORD || "securepassword",
 };
 
-const adminUiRouter = AdminJSExpress.buildAuthenticatedRouter(adminJs, {
-  authenticate: async (email, password) => {
-    if (email === ADMIN.email && password === ADMIN.password) {
-      return ADMIN;
-    }
-    return null;
-  },
-  cookieName: "adminjs",
-  cookiePassword: process.env.ADMIN_COOKIE_SECRET || "supersecret",
-});
+// const adminUiRouter = AdminJSExpress.buildAuthenticatedRouter(adminJs, {
+//   authenticate: async (email, password) => {
+//     if (email === ADMIN.email && password === ADMIN.password) {
+//       return ADMIN;
+//     }
+//     return null;
+//   },
+//   cookieName: "adminjs",
+//   cookiePassword: process.env.ADMIN_COOKIE_SECRET || "supersecret",
+// });
+
+const adminUiRouter = AdminJSExpress.buildRouter(adminJs);
 
 export { adminJs, adminUiRouter };
