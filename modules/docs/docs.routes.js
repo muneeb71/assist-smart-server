fcad7b4064aeedc82df532ee2cf6481d9d51e08b
muneeb71 <ahmedmuneeb183@gmail.com>
2025-07-23 01:23:47 +0500

    feat: auth middleware added

--- modules/docs/docs.routes.js
+++ modules/docs/docs.routes.js
@@ -1,12 +1,13 @@
 import express from "express";
 import * as docsController from "./docs.controller.js";
+import { authenticate } from "../../middlewares/auth.js";
 
 const router = express.Router();
 
-router.get("/", docsController.listDocuments);
-router.get("/:id", docsController.getDocument);
-router.delete("/:id", docsController.deleteDocument);
-router.post("/stream", docsController.streamDocument);
-router.put("/:id", docsController.updateDocument);
+router.get("/", authenticate, docsController.listDocuments);
+router.get("/:id", authenticate, docsController.getDocument);
+router.delete("/:id", authenticate, docsController.deleteDocument);
+router.post("/stream", authenticate, docsController.streamDocument);
+router.put("/:id", authenticate, docsController.updateDocument);
 
 export default router;
